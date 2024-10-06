ee7f45a92875980c721decdd77fa66b8abffa677
kommthom <tbenninghaus@gmx.de>
2024-10-06 23:04:46 +0200

    First draft

--- Package.swift
+++ Package.swift
@@ -5,17 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "UserModels",
+	platforms: [
+			.iOS(.v18),
+			.macOS(.v15),
+			.tvOS(.v16),
+			.watchOS(.v8),
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UserModels",
             targets: ["UserModels"]),
     ],
+	dependencies: [
+			.package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0")
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UserModels"),
+            name: "UserModels",
+			dependencies: [
+				.product(
+					name: "Tagged",
+					package: "swift-tagged"
+				)
+			]
+		),
         .testTarget(
             name: "UserModelsTests",
             dependencies: ["UserModels"]
